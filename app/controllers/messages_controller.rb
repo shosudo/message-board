33fc3c5c9703965246e0e114cfb7d48b9f3ba8dc
Sho Sudo <sudosho1101@gmail.com>
2017-01-08 14:48:27 +0000

    created message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    #@--- = インスタンス変数
+    @message = Message.new
+    #Get all messages
+    @messages = Message.all
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notice:'saved a message'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
   end
 end
