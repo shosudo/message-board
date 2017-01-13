7ef77157c90f19f624354fd3f6aa2c8cd386d273
Sho Sudo <sudosho1101@gmail.com>
2017-01-13 04:38:02 +0000

    create edit function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,9 @@ class MessagesController < ApplicationController
 #ここで対応しているのは views/messages/index.html
 #Controllerの名前の最後の部分は複数形にする. ex) ClientsController, SiteAdminsController
   
+  before_action :set_message, only: [:edit, :update]
+  #:edit,#updateのアクションの実行前に、set_message methodを行う
+  
   def index
     @message = Message.new
     #@--- = インスタンス変数
@@ -24,13 +27,32 @@ class MessagesController < ApplicationController
       #Templateに利用するMessage.allを@messagesに渡す
       flash.now[:alert] = "Failed to save message"
       #エラーメッセージをflash.now[:alert]に代入
+      #エラーメッセージの内容はどこで設定されている??? validationはmessage.rbだが。
+      #messageのcssはmessages.scssで設定
       render 'index'
     end
-    
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はTOPへリダイレクト
+      redirect_to root_path, notice: 'Edit the Message'
+    else
+      #失敗した場合は編集画面へ
+      render 'edit'
+    end
   end
   
   private
   def message_params
     params.require(:message).permit(:name,:body)
+  end
+  
+  def set_message
+    # @mmesageに編集しようとする投稿の内容をセット ??? params[:id] => URLで入力された@messageのparameter idが入力される
+    @message = Message.find(params[:id])
   end
 end
